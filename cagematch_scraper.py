cd9896a20271a1f0ca1a6e450344361e0c1a8efa
Ishaan Sharma <roarkesir@gmail.com>
2024-01-29 14:50:15 +0530

    func to create wrestlers query params

--- cagematch_scraper.py
+++ cagematch_scraper.py
@@ -1,3 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+def create_wrestlers_query(wrestlers):
+    participants = [''] * 4
+    for i in range(min(4, len(wrestlers))):
+        participants[i] = wrestlers[i].replace(' ', '+')
+    query = '&'.join([f'sParticipant{i+1}={p}' for i, p in enumerate(participants)])
+    return query
+
