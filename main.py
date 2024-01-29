a7818768e44d0263fa4b63f5fb12c30b38e19f47
Ishaan Sharma <roarkesir@gmail.com>
2024-01-29 19:06:10 +0530

    add functions to handle embed creation and taking user input

---
+++ main.py
@@ -0,0 +1,73 @@
+# This example requires the 'message_content' intent.
+
+import discord
+from discord import app_commands
+import cagematch_scraper
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+@tree.command(
+    name="suggestmatch", description="Suggest a match between two or more wrestlers!"
+)
+async def suggest_match(ctx, wrestlers: str, after_year: int, before_year: int):
+    try:
+        await ctx.response.defer()
+        wrestlers = wrestlers.split(",")
+        print(wrestlers)
+        print(after_year)
+        print(before_year)
+        wrestling_matches = cagematch_scraper.get_wrestling_matches(
+            wrestlers, after_year, before_year
+        )
+        random_match = cagematch_scraper.pick_random_match(wrestling_matches)
+        match_info = cagematch_scraper.extract_match_info(random_match)
+        # print(r)
+        await ctx.followup.send(embed=match_embed(ctx, match_info))
+    except Exception:
+        await ctx.followup.send(
+            embed=match_not_found_embed(ctx, wrestlers, after_year, before_year)
+        )
+
+
+def match_not_found_embed(ctx, wrestlers, after_year, before_year):
+    wrestlers = ", ".join(wrestlers)
+    embed = discord.Embed(
+        title="Match Not Found",
+        description=f"No match found that includes {wrestlers} between {before_year}-{after_year}. Make sure there are no typing errors or try broadening your search!",
+        color=0xFF0000,
+    )
+    return embed
+
+
+def match_embed(ctx, match):
+    embed = discord.Embed(
+        title="Match Suggestion", description=match["match_details"], color=0x00FF00
+    )
+    embed.add_field(name="Date", value=match["date"], inline=True)
+    embed.add_field(name="Event", value=match["event"], inline=True)
+    embed.add_field(name="Promotion", value=match["promotion"], inline=True)
+    embed.set_thumbnail(url=match["image_link"])
+    print(match["image_link"])
+    return embed
+
+
+client.run("MTIwMTQ3MjA1NTA4Njk0MDI0MA.G0okY4.5bAHyA-g8vquiIxQw8sPOHoKewZhPk9pkigqUY")
