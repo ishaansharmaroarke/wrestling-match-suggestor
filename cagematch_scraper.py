5ccf4da5dc602f607d055e59665682a810b8521c
Ishaan Sharma <roarkesir@gmail.com>
2024-01-29 14:52:12 +0530

    func to get wrestling matches url

--- cagematch_scraper.py
+++ cagematch_scraper.py
@@ -8,3 +8,8 @@ def create_wrestlers_query(wrestlers):
     query = '&'.join([f'sParticipant{i+1}={p}' for i, p in enumerate(participants)])
     return query
 
+def get_wrestling_match(wrestlers, before_year, after_year):
+    contains_wrestlers = create_wrestlers_query(wrestlers)
+
+    URL = f"https://www.cagematch.net/?id=112&view=search&{contains_wrestlers}&sEventName=&sEventType=TV-Show%7CPay+Per+View%7CPremium+Live+Event%7COnline+Stream&sDateFromDay=01&sDateFromMonth=01&sDateFromYear={before_year}&sDateTillDay=31&sDateTillMonth=12&sDateTillYear={after_year}&sPromotion=&sLocation=&sArena=&sRegion=&sMatchType=&sConstellation=&sWorkerRelationship=Any&sFulltextSearch="
+    print(URL)
